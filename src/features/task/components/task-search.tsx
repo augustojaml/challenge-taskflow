85b4694091c09b5384451678ecd010bd6b4d52a0
augustojaml <jamonteirolima@gmail.com>
2025-12-17 12:47:19 -0300

    fix: refactory task serach

--- src/features/task/components/task-search.tsx
+++ src/features/task/components/task-search.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { useEffect, useState } from 'react'
 
 import { Input } from '@/shared/components'
@@ -20,7 +22,6 @@ const TaskSearch = ({ onChange }: TaskSearchProps) => {
 
   const debouncedSearch = useDebounce(search, 500)
 
-  // 🔔 notifica o componente pai
   useEffect(() => {
     onChange({
       search: debouncedSearch,
@@ -29,29 +30,30 @@ const TaskSearch = ({ onChange }: TaskSearchProps) => {
   }, [debouncedSearch, status, onChange])
 
   return (
-    <div className="mb-4 flex gap-4">
-      <Input
-        id="search"
-        type="text"
-        placeholder="Pesquisar tarefa"
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
-        className="h-10 w-fit"
-      />
+    <div className="mb-4 flex flex-col gap-4 md:flex-row md:items-end md:justify-between">
+      <div className="flex flex-1 flex-col gap-4 md:flex-row md:items-center">
+        <Input
+          id="search"
+          type="text"
+          placeholder="Pesquisar tarefa"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          className="h-10 w-fit"
+        />
 
-      <div className="h-12 space-y-1.5">
-        <Select value={status} onValueChange={setStatus}>
-          <SelectTrigger className="h-10!">
-            <SelectValue placeholder="Selecione o status" />
-          </SelectTrigger>
-
-          <SelectContent>
-            <SelectItem value="ALL">Todas as Categorias</SelectItem>
-            <SelectItem value="PENDING">Pendente</SelectItem>
-            <SelectItem value="IN_PROGRESS">Em andamento</SelectItem>
-            <SelectItem value="COMPLETED">Concluída</SelectItem>
-          </SelectContent>
-        </Select>
+        <div className="h-12 space-y-1.5 md:h-auto">
+          <Select value={status} onValueChange={setStatus}>
+            <SelectTrigger className="h-10!">
+              <SelectValue placeholder="Selecione o status" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="ALL">Todas as categorias</SelectItem>
+              <SelectItem value="PENDING">Pendente</SelectItem>
+              <SelectItem value="IN_PROGRESS">Em andamento</SelectItem>
+              <SelectItem value="COMPLETED">Concluída</SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
       </div>
     </div>
   )
